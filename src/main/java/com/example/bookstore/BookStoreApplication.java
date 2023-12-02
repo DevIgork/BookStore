80797545325e1bf63e21ef6a57725956937a45a6
Igor Korolevich <igorkorolj@gmail.com>
2023-12-02 21:01:43 +0200

    added exception, model book, repository, service, configure database access, configure checkstyle, configure CI (Continuous Integration)

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,13 +1,30 @@
 package com.example.bookstore;
 
+import com.example.bookstore.model.Book;
+import com.example.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book harry = new Book();
+            harry.setTitle("harry Potter");
+            bookService.save(harry);
+            System.out.println(bookService.findAll());
+        };
+    }
 
 }
