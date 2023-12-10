0722201fb0b584a0e114c142f0ab627a03abb20f
Igor Korolevich <igorkorolj@gmail.com>
2023-12-10 21:39:42 +0200

    added JpaRepository, auto property validate, Implement soft delete

--- src/main/java/com/example/bookstore/repository/impl/BookDaoImpl.java
+++ src/main/java/com/example/bookstore/repository/impl/BookDaoImpl.java
@@ -1,64 +1,64 @@
-package com.example.bookstore.repository.impl;
-
-import com.example.bookstore.exception.DataProcessingException;
-import com.example.bookstore.model.Book;
-import com.example.bookstore.repository.BookRepository;
-import java.util.List;
-import java.util.Optional;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class BookDaoImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
-    @Override
-    public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.save(book);
-            transaction.commit();
-            session.close();
-            return book;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new DataProcessingException("can't add Data", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-    }
-
-    @Override
-    public Optional<Book> findById(Long id) {
-        try (Session session = sessionFactory.openSession()) {
-            Book book = session.find(Book.class, id);
-            return book != null ? Optional.of(book) : Optional.empty();
-        } catch (Exception e) {
-            throw new DataProcessingException("Cant find data by id from book", e);
-        }
-    }
-
-    @Override
-    public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("SELECT b FROM Book b", Book.class).getResultList();
-        } catch (Exception e) {
-            throw new DataProcessingException("Cant find all data from book", e);
-        }
-    }
-}
+//package com.example.bookstore.repository.impl;
+//
+//import com.example.bookstore.exception.DataProcessingException;
+//import com.example.bookstore.model.Book;
+//import com.example.bookstore.repository.BookRepository;
+//import java.util.List;
+//import java.util.Optional;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class BookDaoImpl implements BookRepository {
+//    private final SessionFactory sessionFactory;
+//
+//    @Autowired
+//    public BookDaoImpl(SessionFactory sessionFactory) {
+//        this.sessionFactory = sessionFactory;
+//    }
+//
+//    @Override
+//    public Book save(Book book) {
+//        Session session = null;
+//        Transaction transaction = null;
+//        try {
+//            session = sessionFactory.openSession();
+//            transaction = session.beginTransaction();
+//            session.save(book);
+//            transaction.commit();
+//            session.close();
+//            return book;
+//        } catch (Exception e) {
+//            if (transaction != null) {
+//                transaction.rollback();
+//            }
+//            throw new DataProcessingException("can't add Data", e);
+//        } finally {
+//            if (session != null) {
+//                session.close();
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public Optional<Book> findById(Long id) {
+//        try (Session session = sessionFactory.openSession()) {
+//            Book book = session.find(Book.class, id);
+//            return book != null ? Optional.of(book) : Optional.empty();
+//        } catch (Exception e) {
+//            throw new DataProcessingException("Cant find data by id from book", e);
+//        }
+//    }
+//
+//    @Override
+//    public List<Book> findAll() {
+//        try (Session session = sessionFactory.openSession()) {
+//            return session.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+//        } catch (Exception e) {
+//            throw new DataProcessingException("Cant find all data from book", e);
+//        }
+//    }
+//}
