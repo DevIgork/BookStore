0435d97e308597619b5928d69565df2de0c526b4
Igor Korolevich <igorkorolj@gmail.com>
2023-12-13 17:54:43 +0200

    fixed check

--- src/main/java/com/example/bookstore/repository/BookRepository.java
+++ src/main/java/com/example/bookstore/repository/BookRepository.java
@@ -3,8 +3,6 @@ package com.example.bookstore.repository;
 import com.example.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
