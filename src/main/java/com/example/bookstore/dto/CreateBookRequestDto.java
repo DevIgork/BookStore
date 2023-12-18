55041226c78297341ab58ad89a74b5156fbbeb8a
Igor Korolevich <igorkorolj@gmail.com>
2023-12-18 22:48:23 +0200

    added check for blank and changet isbn check

--- src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,7 +1,9 @@
 package com.example.bookstore.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -9,13 +11,17 @@ import lombok.Data;
 @Data
 public class CreateBookRequestDto {
     @NotNull(message = " value can't be null")
+    @NotBlank(message = "value can't be blank")
     private String title;
     @NotNull(message = " value can't be null")
+    @NotBlank(message = "value can't be blank")
     private String author;
     @NotNull(message = " value can't be null")
-    @Size(min = 10, max = 13,message = " value can't be lower than 10 or more then 13")
+    @NotBlank(message = "value can't be blank")
+    @Pattern(regexp = "^(10|13)$", message = "value size must be 10 or 13")
     private String isbn;
     @NotNull(message = " value can't be null")
+    @NotBlank(message = "value can't be blank")
     @Min(0)
     private BigDecimal price;
     private String description;
