06345806934c972b95bda992ece64ff57e1d2759
Igor Korolevich <igorkorolj@gmail.com>
2023-12-20 10:18:09 +0200

    removed unused imports

--- src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,6 +1,5 @@
 package com.example.bookstore.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
