dcbbfc49cd4f7a67574431a0d9b0020a350da39c
Igor Korolevich <igorkorolj@gmail.com>
2023-12-20 10:06:40 +0200

    removed unused imports

--- src/main/java/com/example/bookstore/dto/BookDto.java
+++ src/main/java/com/example/bookstore/dto/BookDto.java
@@ -1,11 +1,7 @@
 package com.example.bookstore.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
-import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookDto {
