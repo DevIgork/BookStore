1c02515e0ed3a9c3284cf80dac1ca5eb5019b2c2
Igor Korolevich <igorkorolj@gmail.com>
2023-12-26 10:47:30 +0200

    structured my code added work with jwt token, added login to auth

---
+++ src/main/java/com/example/bookstore/model/Category.java
@@ -0,0 +1,22 @@
+package com.example.bookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String name;
+    private String description;
+}
