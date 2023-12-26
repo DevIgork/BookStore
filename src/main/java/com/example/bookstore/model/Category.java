12583e0965109f024846accdfb9565391dc58860
Igor Korolevich <igorkorolj@gmail.com>
2023-12-26 14:01:52 +0200

    added Category.java yaml create table Category book-category

--- src/main/java/com/example/bookstore/model/Category.java
+++ src/main/java/com/example/bookstore/model/Category.java
@@ -6,17 +6,22 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty
+    @Column(nullable = false)
     private String name;
     private String description;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
