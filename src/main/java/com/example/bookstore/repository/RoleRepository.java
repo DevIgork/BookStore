a6681503dc4c3c16799bfdb3d000f8673e83f155
Igor Korolevich <igorkorolj@gmail.com>
2023-12-26 10:47:41 +0200

    structured my code added work with jwt token, added login to auth

--- src/main/java/com/example/bookstore/repository/RoleRepository.java
+++ src/main/java/com/example/bookstore/repository/RoleRepository.java
@@ -3,8 +3,9 @@ package com.example.bookstore.repository;
 import com.example.bookstore.model.Role;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    @Override
-    Optional<Role> findById(Long id);
+    Optional<Role> findByRole(Role.RoleName roleName);
 }
