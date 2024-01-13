992af345005e5b686a4c7100c65bcbcac8bf57ba
Igor Korolevich <igorkorolj@gmail.com>
2024-01-13 16:38:33 +0200

    added @IterableMapping, added some private methods to OrderServiceImpl

--- src/main/java/com/example/bookstore/mapper/OrderMapper.java
+++ src/main/java/com/example/bookstore/mapper/OrderMapper.java
@@ -3,11 +3,19 @@ package com.example.bookstore.mapper;
 import com.example.bookstore.config.MapperConfig;
 import com.example.bookstore.dto.order.OrderDto;
 import com.example.bookstore.model.Order;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(config = MapperConfig.class, uses = {OrderItemMapper.class})
 public interface OrderMapper {
+    @Named("toDto")
     @Mapping(target = "userId", source = "user.id")
     OrderDto toDto(Order order);
+
+    @IterableMapping(qualifiedByName = "toDto")
+    List<OrderDto> toDtos(List<Order> orders);
 }
